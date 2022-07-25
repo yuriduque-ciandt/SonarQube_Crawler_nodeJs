5b63551e94c0cf7547d0777023137d0fbf010031
souzadaniel <eu@danielsouza.dev.br>
2022-07-25 14:57:54 -0300

    feat: listing repositories and improving details of measures

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -44,18 +44,46 @@ router.get("/list", async (_, res) => {
 
     const keyComponents = fetchedComponents.map(({ key }) => key);
 
+    const metricsKeys = [
+      "bugs",
+      "vulnerabilities",
+      "security_hotspots_reviewed",
+      "code_smells",
+      "coverage",
+      "duplicated_lines_density",
+    ];
+
+    let {
+      data: { metrics },
+    } = await instance.get("/metrics/search", { params: { ps: 200 } });
+    metrics = metrics.filter(({ key }) => metricsKeys.includes(key));
+    metrics = metrics.map(({ key, type, name, direction }) => ({
+      key,
+      type,
+      name,
+      direction,
+    }));
+
     const data = await Promise.all(
       keyComponents.map(async (key) => {
-        const {
-          data: {
-            component: { measures },
-          },
-        } = await instance.get(`/measures/component`, {
+        const { data } = await instance.get(`/measures/component`, {
           params: {
-            metricKeys:
-              "bugs,vulnerabilities,security_hotspots_reviewed,code_smells,coverage,duplicated_lines_density",
+            metricKeys: metrics.map(({ key }) => key).join(","),
             component: key,
           },
+        });
+
+        let {
+          component: { measures },
+        } = data;
+        measures = measures.map(({ metric, value, bestValue }) => {
+          const data = metrics.find(({ key }) => metric === key);
+
+          return {
+            ...data,
+            value,
+            bestValue,
+          };
         });
 
         return {
