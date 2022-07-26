871c9b1b8c991ac2ac566ba2f3f372f8c5125dbe
yurid <yurid@ciandt.com>
2022-07-26 16:29:37 -0300

    implement logs

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,6 @@
-import projectRoutes from "./projectRoutes.js";
 import sonarRoutes from "./sonarRoutes.js";
 
 const buildRoutes = (app) => {
-  app.use("/api/project", projectRoutes);
   app.use("/api/sonar", sonarRoutes);
 };
 
