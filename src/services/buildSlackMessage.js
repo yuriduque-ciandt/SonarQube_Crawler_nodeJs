ebda2992e3af8f9e593cc9912bf375b20b9b7c5e
yurid <yurid@ciandt.com>
2022-08-16 14:59:01 -0300

    fix lbal branch link

--- src/services/buildSlackMessage.js
+++ src/services/buildSlackMessage.js
@@ -20,7 +20,7 @@ function setMessageHeader(body, message) {
     text: {
       type: "plain_text",
       text: `${body.project.name}: ${
-        body.status == "SUCCESS" ? successIcon : errorIcon
+        body.qualityGate.status == "SUCCESS" ? successIcon : errorIcon
       }`,
       emoji: true,
     },
@@ -31,7 +31,7 @@ function setMessageHeader(body, message) {
       {
         type: "mrkdwn",
         text: `The analysis for project '${body.project.name}' ${
-          body.status == "SUCCESS" ? "success" : "failed"
+          body.qualityGate.status == "SUCCESS" ? "success" : "failed"
         }`,
       },
     ],
